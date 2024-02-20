0af30a0b7d9f396941706892ab7ae179981200e4
jrunge <jrunge@precisionplanting.com>
2024-02-19 22:23:30 -0600

    Lexer done

--- main.cpp
+++ main.cpp
@@ -17,9 +17,12 @@ static double NumVal;               // Filled in if tok_number
 
 static int gettok() {
     static int LastChar = ' ';
+
+    // Skip any whitespace
     while(isspace(LastChar))
         LastChar = getchar();
 
+    // Handle identifiers and literals
     if(isalpha(LastChar)) {
         IdentifierStr = LastChar;
 
@@ -31,4 +34,35 @@ static int gettok() {
         }
         return tok_identifier;
     }
+
+    // Handle numbers
+    if(isdigit(LastChar) || LastChar == '.') {
+        std::string NumStr;
+        do {
+            NumStr += LastChar;
+            LastChar = getchar();
+        } while (isdigit(LastChar) || LastChar == '.');
+
+        NumVal = strtod(NumStr.c_str(), 0);
+        return tok_number;
+    }
+
+    // Handle comments
+    if(LastChar == '#') {
+        do 
+            LastChar = getchar();
+        while (LastChar != EOF && LastChar != '\n' && LastChar != '\r');
+
+        if(LastChar != EOF)
+            return gettok();
+    }
+
+    // Check for end of file. Don't eat the EOF
+    if(LastChar == EOF)
+        return tok_eof;
+
+    // Otherwise, just return the character as its ascii value
+    int ThisChar = LastChar;
+    LastChar = getchar();
+    return ThisChar;
 }
